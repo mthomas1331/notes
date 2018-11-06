508dd9689e77e57107d1475e2693d1f9c9e3dcd7
mthomas1331 <mthomas6298@outlook.com>
2018-11-06 14:54:00 +0000

    Added notes title index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :notes
   root 'home#home'
 
   get 'contact', to:'home#contact'
---
+++ db/migrate/20181106144102_create_notes.rb
@@ -0,0 +1,10 @@
+class CreateNotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notes do |t|
+      t.string :title, null: false
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
